e63b32d39927cf44e66da7ec653553141d28060a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-05-21 14:31:41 +0900

    New TaliwindAtb.css

--- src/luna.JS/TaliwindAtb.js/TaliwindATB.js
+++ src/luna.JS/TaliwindAtb.js/TaliwindATB.js
@@ -1,1 +1,12 @@
-// Atb => Class Convert
+window.onload = function() {
+  let elements = document.querySelectorAll("*");
+
+  for (let i = 0; i < elements.length; i++) {
+    let attributes = elements[i].getAttributeNames();
+    for (let j = 0; j < attributes.length; j++) {
+      let attribute = attributes[j];
+      elements[i].classList.add(attribute);
+    }
+  }
+}
+  
