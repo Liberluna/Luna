c4c27a551586f907a35cf761b082a2766d168183
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-05-21 15:37:56 +0900

    Make TailwindATB.js

--- src/luna.JS/TaliwindAtb.js/TaliwindATB.js
+++ src/luna.JS/TaliwindAtb.js/TaliwindATB.js
@@ -1,12 +1,30 @@
-window.onload = function() {
-  let elements = document.querySelectorAll("*");
+function TaliwindATB() {
+    let elements = document.querySelectorAll("*");
 
-  for (let i = 0; i < elements.length; i++) {
-    let attributes = elements[i].getAttributeNames();
-    for (let j = 0; j < attributes.length; j++) {
-      let attribute = attributes[j];
-      elements[i].classList.add(attribute);
+    for (let i = 0; i < elements.length; i++) {
+        let attributes = elements[i].getAttributeNames();
+        elements[i].className = "";
+        for (let j = 0; j < attributes.length; j++) {
+            let attribute = attributes[j];
+            if (attribute !== "class" && attribute !== "id" && attribute !== "style" && attribute !== null) {
+                elements[i].classList.add(attribute);
+            }
+        }
     }
-  }
 }
-  
+
+function Cdf() {
+    TaliwindATB();
+}
+
+window.onload = function () {
+    const observer = new MutationObserver((mutationsList) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList' && mutation.target.nodeName === 'BODY') {
+                Cdf();
+            }
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+}
