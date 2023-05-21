0119dcde8bd9e8779f31947c2dde9e1077c9a6d0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-05-21 13:49:15 +0900

    Update All

--- src/luna.JS/Component.js/Cmp.js
+++ src/luna.JS/Component.js/Cmp.js
@@ -1,80 +1,94 @@
 var Luna = {
-    Cmp: {
-        Build: function () {
-            let _Cmps = document.querySelectorAll('[cmp]');
-            for (let i = 0; i < _Cmps.length; i++) {
-                fetch(_Cmps[i].getAttribute('cmp'))
-                    .then(response => response.text())
-                    .then(data => {
-                        data = Luna.Ls.TmpProp(data, _Cmps[i].getAttribute('slot'));
-                        _Cmps[i].innerHTML = data.replaceAll("File not found","Luna : Cmp Undefind").replaceAll(" cannot be found. It may have been moved, edited, or deleted."," : Undefind");
-                    })
-                    .catch(error => {
-                        Luna.Csl.Error(error);
-                    });
-            }
-        }
-    },
-    Csl: {
-        Warn: function (msg) {
-            console.warn('Luna : ', msg);
-        },
-        Error: function (msg) {
-            console.error('Luna : ', msg);
-        },
-        Info: function (msg) {
-            console.info('Luna : ', msg);
-        },
-        Log: function (msg) {
-            console.log('Luna : ', msg);
-        },
-        Clear: function () {
-            console.clear();
-        }
-    },
-    Ls: {
-        GetProp: function (key, box) {
-            if (typeof key) {
-                if (!typeof box) {
-                    return undefined;
-                }
-                let key_box = Function('return ' + box)();
-                /*
-                    box => 連想配列 => key取得
-                    key_box[key] => 値
-                */
-                return key_box[key];
-            }
-            return Function('return ' + key)();
-        },
-        TmpProp: function (str, box) {
+  Cmp: {
+    Build: function (Selector) {
+      if (typeof Selector == 'undefined') {
+        Selector = "";
+      }
 
-                const regex = /{{\s*([\w.]+)\s*}}/g;
-                
-                const result = str.replace(regex, (match, propName) => {
-
-                  const value = Luna.Ls.GetProp(propName, box);
-                  
-                  return value !== undefined ? value : match;
-                });
-                
-                return result;
-        }
-    },
-    User: {
-        Set: function (func, name) {
-            Luna.User[name] = func; // 関数に変換
-        }
+      let _Cmps = document.querySelectorAll(Selector + "[cmp]");
+      for (let i = 0; i < _Cmps.length; i++) {
+        fetch(_Cmps[i].getAttribute('cmp'))
+          .then(response => response.text())
+          .then(data => {
+            data = Luna.Ls.TmpProp(data, _Cmps[i].getAttribute('slot'));
+            _Cmps[i].innerHTML = data
+              .replaceAll("File not found", "Luna : Cmp Undefind")
+              .replaceAll(
+                " cannot be found. It may have been moved, edited, or deleted.",
+                " : Undefind"
+              );
+          })
+          .catch(error => {
+            Luna.Csl.Error(error);
+          });
+      }
     }
+  },
+  Csl: {
+    Warn: function (msg) {
+      console.warn('Luna : ', msg);
+    },
+    Error: function (msg) {
+      console.error('Luna : ', msg);
+    },
+    Info: function (msg) {
+      console.info('Luna : ', msg);
+    },
+    Log: function (msg) {
+      console.log('Luna : ', msg);
+    },
+    Clear: function () {
+      console.clear();
+    }
+  },
+  Ls: {
+    GetProp: function (key, box) {
+      if (typeof key === 'string') {
+        if (typeof box !== 'undefined') {
+          let key_box = Function('return ' + box)();
+          return key_box[key];
+        }
+        return Function('return ' + key)();
+      }
+      return undefined;
+    },
+    TmpProp: function (str, box) {
+      const regex = /{{\s*([\w.]+)\s*}}/g;
+      const result = str.replace(regex, (match, propName) => {
+        const value = Luna.Ls.GetProp(propName, box);
+        return value !== undefined ? value : match;
+      });
+      return result;
+    }
+  },
+  User: {
+    Set: function (func, name) {
+      if (typeof Luna.User[name]) {
+
+      }
+
+      if (typeof func === 'function') {
+        Luna.User[name] = func;
+      } else if (typeof func === 'string') {
+        let funcStr = 'Luna.User.' + name + ' = ' + func;
+        Luna.Ls.GetProp(funcStr);
+      } else {
+        console.error('Luna : Invalid function.');
+      }
+    },
+    Delete: function (name) {
+      delete Luna.User[name];
+    }
+  }
 };
 
-//let name = "test"
 Luna.Cmp.Build();
 
-Luna.User.Set(`
-    function () {
-        console.log("test");
-    }
-`, "test")
+Luna.User.Set(
+  function test() {
+    console.log("tests");
+  },
+  "test"
+);
 
-//Luna.User.test();
+Luna.User.test();
---
+++ src/luna.JS/TaliwindAtb.js/TaliwindATB.js
@@ -0,0 +1,1 @@
+// Atb => Class Convert
